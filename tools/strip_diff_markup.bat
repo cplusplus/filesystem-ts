b637163c0de532f1482424975c64e35b01c0f37f
Beman <bdawes@acm.org>
2014-07-03 15:40:27 -0400

    Initial commit of tool to help with diff-markup removal. Hand pass over working-draft-diffless.html to handle cases not handled correctly by tool and other boo boos.

--- tools/strip_diff_markup.bat
+++ tools/strip_diff_markup.bat
@@ -1,1 +1,5 @@
 sed "s~<ins>~~g" working-draft.html >working-draft-diffless.html
+sed -i "s~</ins>~~g" working-draft-diffless.html
+sed -i "s~<del>.*</del>~~g" working-draft-diffless.html
+chg working-draft-diffless.html "\n" "\r\n"
+grep -n "<del>" working-draft-diffless.html
