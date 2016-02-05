955265e31c219b0829a4e4691277f2dc249df11d
Beman <bdawes@acm.org>
2016-02-05 09:27:31 -0500

    Clean markup boo boos.

--- c++17/build-html.bat
+++ c++17/build-html.bat
@@ -1,4 +1,7 @@
 @echo off
 pandoc --standalone --self-contained --css=styles.css --title-prefix="C++17 Filesystem" -f markdown_github -t html -o c++17-proposal.temp c++17-proposal.md
 html_include_files c++17-proposal.temp c++17-proposal.html
+
+if %1$ == --noshow$ goto done
 c++17-proposal.html
+:done
